e05dfccd9c9feba8d612944f1cc31b1cd5d4b368
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-05-21 08:29:31 +0300

    Revert "feature: added PopUpWithImage class, ref Card, Popup and index.js"
    This reverts commit a3ad866c6aa7ee57d7494bd694bb7f4dc2e1e431.

--- scripts/components/Card.js
+++ scripts/components/Card.js
@@ -1,9 +1,9 @@
 class Card {
-    constructor({ data, handleCardClick }, cardSelector) {
+    constructor(data, cardSelector, openZoomPopup) {
         this._name = data.name;
         this._link = data.link;
         this._cardSelector = cardSelector;
-        this._handleCardClick = handleCardClick;
+        this._openZoomPopup = openZoomPopup;
     }
     
     _getTemplate() {
@@ -18,19 +18,18 @@ class Card {
 
     generateCard() {
         this._element = this._getTemplate();
-        this._photo = this._element.querySelector('.card__photo');
         this._setEventListeners();
 
-        this._photo.src = this._link;
-        this._photo.alt = this._name;
+        this._element.querySelector('.card__photo').src = this._link;
+        this._element.querySelector('.card__photo').alt = this._name;
         this._element.querySelector('.card__caption').textContent = this._name;
 
         return this._element;
     }
 
     _setEventListeners() {
-        this._photo.addEventListener('click', () => { 
-            this._handleCardClick(this._name, this._link);
+        this._element.querySelector('.card__photo').addEventListener('click', () => { 
+            this._openZoomPopup(this._name, this._link);
         });
 
         this._element.querySelector('.card__like').addEventListener('click', () => { 
